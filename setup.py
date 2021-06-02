37ad7087236fe4107fe335ef8abfb742f11c6940
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-06-01 21:17:42 -0400

    Added count

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="relations-sqlite3",
-    version="0.1.4",
+    version="0.1.5",
     package_dir = {'': 'lib'},
     py_modules = [
         'relations_sqlite3'
