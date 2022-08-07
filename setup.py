0fb40100b00844f23064225843d5dccf976f6c1d
Gaffer Fitch <gaffer.fitch@gmail.com>
2022-08-07 16:58:52 -0400

    Added PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
 setup(
-    name="python-relations-sqlite3",
-    version="0.6.5",
+    name="relations-sqlite3",
+    version="0.6.6",
     package_dir = {'': 'lib'},
     py_modules = [
         'relations_sqlite3'
     ],
-    install_requires=[]
+    install_requires=[
+        'relations-sqlite>=0.6.1'
+    ],
+    url="https://github.com/relations-dil/python-relations-sqlite3",
+    author="Gaffer Fitch",
+    author_email="relations@gaf3.com",
+    description="DB Modeling for SQLite using the sqlite3 library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license_files=('LICENSE.txt',),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
